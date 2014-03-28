2e17c0f36a787c7b0e4f39677cac8bb0749e5a99
Manaal Faruqui <manaalfar@gmail.com>
2014-03-28 01:13:16 -0400

    enriching code with automatic parameter

--- src/enrich.py
+++ src/enrich.py
@@ -4,18 +4,19 @@ import numpy
 from io import read_word_vecs, print_word_vecs
 from io import read_ppdb
 
-def euclidean(wordVecs, ppDict, iter, alpha):
+def euclidean(wordVecs, ppDict, iter):
   
   for word in ppDict:
     if word in wordVecs:
-      newVec = alpha * wordVecs[word]
-      countPPWords = 0
+      countPPWords = len([ppWord for ppWord in ppDict[word] if ppWord in wordVecs])
+      newVec = countPPWords * wordVecs[word]
       for ppWord in ppDict[word]:
         if ppWord in wordVecs:
           newVec += wordVecs[ppWord]
           countPPWords += 1
-      newVec /= (alpha + countPPWords)
-      wordVecs[word] = newVec  
+      if countPPWords > 0:
+      	newVec /= 2*countPPWords
+      	wordVecs[word] = newVec  
   return wordVecs
   
 if __name__=='__main__':
@@ -23,8 +24,9 @@ if __name__=='__main__':
   wordVecs = read_word_vecs(sys.argv[1])
   ppDict = read_ppdb(sys.argv[2], wordVecs)
   numIter = int(sys.argv[3])
+  outFileName = sys.argv[4]
   
   ''' Enrich the word vectors using ppdb '''
-  newWordVecs = euclidean(wordVecs, ppDict, numIter, 5)
+  newWordVecs = euclidean(wordVecs, ppDict, numIter)
   ''' Print the enriched vectors '''
-  print_word_vecs(wordVecs, 'y.txt')
+  print_word_vecs(wordVecs, outFileName)
